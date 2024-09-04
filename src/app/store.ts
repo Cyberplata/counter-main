467db3fbaf9a5f6e50c54daf9bebabf33e94c299
PlatonKynin <kininplaton@gmail.com>
2024-09-04 21:07:00 +0300

    6.Counter2.0 -> install redux and add store.ts

---
+++ src/app/store.ts
@@ -0,0 +1,31 @@
+import {combineReducers, legacy_createStore} from "redux";
+import {counterStateReducer} from "../model/counterState-reducer";
+
+
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
+    counterState: counterStateReducer,
+})
+// непосредственно создаём store
+export const store = legacy_createStore(rootReducer)
+
+// определить автоматически тип всего объекта состояния
+export type RootState = ReturnType<typeof store.getState>
+// export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store
+
+
+// // Наш объект store
+/*
+{
+    state: {
+        counterState: {},
+    },
+    getState(),
+    dispatch(),
+    subscribe()
+}*/
