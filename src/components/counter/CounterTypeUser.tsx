d5e35a377ec64b1723eef07f5af7315bb4ed83dd
PlatonKynin <kininplaton@gmail.com>
2024-09-04 23:18:31 +0300

    6.Counter2.0 -> Зарефакторил большое количество dispatch. Объединил в один action creator и добавил 2 новых case в редьюсер updateCounterSettingsAC и resetCounterAC

--- src/components/counter/CounterTypeUser.tsx
+++ src/components/counter/CounterTypeUser.tsx
@@ -56,8 +56,6 @@ export const CounterTypeUser = ({counterState, dispatch}: CounterTypeUserType) =
             countUser: 0,
             setButtonDisabled: false
         }))
-
-
     }
 
     return (
